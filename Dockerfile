6033cd3bf05568ef56f816c72f1a58cc5b83ad3c
Cameron Steel <cameron.steel@internet.asn.au>
2026-04-21 10:06:10 +1000

    include upstream xos and nodegrid

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 # Single-stage build of an oxidized container from phusion/baseimage-docker jammy-1.0.1, derived from Ubuntu 22.04 (Jammy Jellyfish)
-FROM oxidized/oxidized:latest
+FROM oxidized/oxidized:0.29.1-168-g5a417f5
 
 COPY asetus.rb /var/lib/gems/3.0.0/gems/asetus-0.4.0/lib/asetus.rb
+COPY xos.rb /var/lib/gems/3.0.0/gems/oxidized-0.29.1/lib/oxidized/model/xos.rb
+COPY nodegrid.rb /var/lib/gems/3.0.0/gems/oxidized-0.29.1/lib/oxidized/model/nodegrid.rb
---
+++ xos.rb
@@ -0,0 +1,60 @@
+class XOS < Oxidized::Model
+  using Refinements
+
+  # Extreme Networks XOS
+
+  prompt /^\s?\*?\s?[-\w]+\s?[-\w.~]+(:\d+)? [#>] $/
+  comment  '# '
+
+  cmd :all do |cfg|
+    # xos inserts leading \r characters and other trailing white space.
+    # this deletes extraneous \r and trailing white space.
+    cfg.each_line.to_a[1..-2].map { |line| line.delete("\r").rstrip }.join("\n") + "\n"
+  end
+
+  cmd 'show version' do |cfg|
+    comment cfg
+  end
+
+  cmd 'show diagnostics' do |cfg|
+    comment cfg
+  end
+
+  cmd 'show licenses' do |cfg|
+    comment cfg
+  end
+
+  cmd 'show switch' do |cfg|
+    cfg.gsub! /Next periodic save on.*/, ''
+    comment cfg.each_line.reject { |line| line.match(/Time:/) || line.match(/boot/i) || line.match(/Next periodic/) }.join
+  end
+
+  cmd 'show configuration' do |cfg|
+    cfg = cfg.each_line.reject { |line| line.match /^#(\s[\w -]+\s)(Configuration generated)/ }.join
+    cfg
+  end
+
+  cmd 'show policy detail' do |cfg|
+    comment cfg
+  end
+
+  cfg :telnet do
+    username /^login:/
+    password /^\r*password:/
+  end
+
+  cfg :telnet, :ssh do
+    post_login do
+      data = cmd 'disable clipaging session'
+      match = data.match /^disable clipaging session\n\r?\*?\s?[-\w]+\s?[-\w.~]+(:\d+)? [#>] $/m
+      next if match
+
+      cmd 'disable clipaging'
+    end
+
+    pre_logout do
+      send "exit\n"
+      send "n\n"
+    end
+  end
+end
